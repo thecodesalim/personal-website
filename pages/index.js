31e81187a3447a4043ab3eae3a0792a0e5c0658e
Salim Abubakar <samatazu@gmail.com>
2022-05-07 22:57:33 +0100

    new favicon

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex pt-5 flex-col h-screen w-full bg-primary text-secondary">
       <div className="flex grow pl-10 pr-10 flex-col">
         <div className="pt-24 self-center">
-          <p className="font-bold text-secondary mb-8">Salim Abubakar</p>
+          <p className="font-medium text-secondary mb-8">Salim Abubakar</p>
           <p className="text-secondary mb-10">Frontend engineer and designer</p>
           <div>
             <p className="text-tertiary text-sm mb-6">Projects</p>
@@ -18,7 +18,7 @@ export default function Home() {
       </div>
       <footer className="flex h-10 w-full bg-primary pl-10">
         <ul className="flex items-center text-xs text-tertiary">
-          <li>© 2021 Salim Abubakar. All rights reserved.</li>
+          <li>Abuja 2020</li>
         </ul>
       </footer>
     </div>
