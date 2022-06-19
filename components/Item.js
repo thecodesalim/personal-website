be4f03b00a79d080d3d67ffc98f5fb2e4e88023a
Salim Abubakar <salimabubakar@Salims-MacBook-Pro.local>
2022-06-19 22:21:23 +0100

    added content

--- components/Item.js
+++ components/Item.js
@@ -1,6 +1,6 @@
 export default function Item({ name, detail, link }) {
   return (
-    <a target="_blank" href={link}>
+    <a target="_blank" href={link} rel="noreferrer">
       <li className="pb-4">
         <p className="text-secondary mb-3 font-light underline">{name}</p>
         <p className="text-tertiary text-sm w-60">{detail}</p>
